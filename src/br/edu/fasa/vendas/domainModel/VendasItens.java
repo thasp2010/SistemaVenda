4ad4e3f1eafaacaa5d5f0e3a9cc7d398f89e98be
Thais <thais.framerelay@gmail.com>
2012-03-18 08:57:36 -0300

    Criado codigo da classe VendasIntens

--- src/br/edu/fasa/vendas/domainModel/VendasItens.java
+++ src/br/edu/fasa/vendas/domainModel/VendasItens.java
@@ -10,4 +10,53 @@ package br.edu.fasa.vendas.domainModel;
  */
 public class VendasItens {
     
+    private int cod;
+    private int venda;
+    private int produto ;
+    private int quantidade;
+    private float valorunitario;
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public int getProduto() {
+        return produto;
+    }
+
+    public void setProduto(int produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float getValorunitario() {
+        return valorunitario;
+    }
+
+    public void setValorunitario(float valorunitario) {
+        this.valorunitario = valorunitario;
+    }
+
+    public int getVenda() {
+        return venda;
+    }
+
+    public void setVenda(int venda) {
+        this.venda = venda;
+    }
+    
+    
+    
+    
 }
