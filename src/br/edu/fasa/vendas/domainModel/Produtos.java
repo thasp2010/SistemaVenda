f3bec84e445b4dc42924daa99038df506fb9fcec
Thais <thais.framerelay@gmail.com>
2012-03-18 08:51:50 -0300

    Adicionado codigo da classe produtos.

--- src/br/edu/fasa/vendas/domainModel/Produtos.java
+++ src/br/edu/fasa/vendas/domainModel/Produtos.java
@@ -10,4 +10,43 @@ package br.edu.fasa.vendas.domainModel;
  */
 public class Produtos {
     
+    private int cod;
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    private String nome;
+    private float valor;
+    private int estoque;
+    
+    
+    
 }
