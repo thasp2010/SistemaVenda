92741f2b130c0297bd0a60cc3d2aab9a706a2bf4
Thais <thais.framerelay@gmail.com>
2012-03-18 08:47:56 -0300

    Foram criadas as classes Clientes, Produtos e ItensVendas.

---
+++ src/br/edu/fasa/vendas/domainModel/Clientes.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fasa.vendas.domainModel;
+
+/**
+ *
+ * @author thais
+ */
+public class Clientes {
+    
+    private int cod;
+    private String nome;
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+    
+    
+}
